9da78100298c6a002c920bb8b17fa28be7d27e1c
italo alcantara <italo.ads31@gmail.com>
2019-04-12 14:20:53 -0300

    Criação da tela de pesquisa de titulos

--- src/main/java/com/italo/cobranca/controller/TituloController.java
+++ src/main/java/com/italo/cobranca/controller/TituloController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,12 @@ public class TituloController {
 		
 		ModelAndView modelAndView = new ModelAndView("CadastroTitulo");
 			
+		return modelAndView;
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar() {
+		ModelAndView modelAndView = new ModelAndView("PesquisaTitulos");
 		return modelAndView;
 	}
 	
